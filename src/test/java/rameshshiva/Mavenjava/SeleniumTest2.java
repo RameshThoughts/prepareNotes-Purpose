dcd6a3a078edbdce86c6e4e50aa1afc4c57fa403
ramesh <rameshshiva68@gmail.com>
2021-03-15 16:50:26 +0530

    final commit

---
+++ src/test/java/rameshshiva/Mavenjava/SeleniumTest2.java
@@ -0,0 +1,22 @@
+package rameshshiva.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class SeleniumTest2 {
+
+	@Test
+	public void SeleniumTest1() {
+		System.out.println("SeleniumTest1");
+	}
+	
+	
+	@Test
+	public void SeleniumTestHai() {
+		System.out.println("SeleniumTest2");
+	}
+	
+	@Test
+	public void SeleniumTestHello() {
+		System.out.println("SeleniumTest2");
+	}
+}
