4afbe4516df702900a707ba0bdb2095705fa06e6
ramesh <rameshshiva68@gmail.com>
2021-03-15 13:52:42 +0530

    ganga branch changes

--- src/test/java/rameshshiva/Mavenjava/RESTAPITest.java
+++ src/test/java/rameshshiva/Mavenjava/RESTAPITest.java
@@ -6,12 +6,15 @@ public class RESTAPITest {
 
 	@Test
 	public void RESTAPITest1() {
+		
+		// changesfrom ganga branch
+		
 		System.out.println("RESTAPITest1");
 		System.out.println("RESTAPITest2");
 		System.out.println("RESTAPITest3");
 		System.out.println("RESTAPITest4");
 		System.out.println("RESTAPITest5");
-		// changesfrom gitstaging
+		
 		
 		System.out.println("RESTAPITest11");
 		System.out.println("RESTAPITest12");
