5d4f1379088a319438670f345f41e8634800bf08
ramesh <rameshshiva68@gmail.com>
2021-03-15 12:08:24 +0530

    git notes prepare practice1 no changes

---
+++ src/test/java/rameshshiva/Mavenjava/RESTAPITest.java
@@ -0,0 +1,17 @@
+package rameshshiva.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class RESTAPITest {
+
+	@Test
+	public void RESTAPITest1() {
+		System.out.println("RESTAPITest1");
+	}
+	
+	
+	@Test
+	public void RESTAPITest2() {
+		System.out.println("RESTAPITest2");
+	}
+}
